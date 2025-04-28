da46698e09b4ff8dd73ec90e2bdefd3c5d4f3b1e
agustinpedrotti <apedrotti1@mail.austral.edu.ar>
2025-04-28 12:16:38 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -11,16 +11,20 @@ def remove_elements(list_to_remove_elements):
    elif len(list_to_remove_elements) < 5:
          list_to_remove_elements.pop(0)
       return list_to_remove_elements
-else:
-   return list_to_remove_elements
+   else:
+      return list_to_remove_elements
 
 
 def add_elements(list_to_add_elements):
-    return 
+    
+   return 
 
 
 def is_empty(list_to_check):
-    return 
+   if len(list_to_check) = 0:
+      return True
+else:
+   return False
 
 def check_lists(list_to_compare1, list_to_compare2):
     return 
